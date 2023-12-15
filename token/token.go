0b1992e0bdbdcf8e89fe436a78fa9f2539acbc3e
Patryk Lizoń <lizon.patryk@gmail.com>
2023-12-15 20:24:31 +0100

    make token package

---
+++ token/token.go
@@ -0,0 +1,29 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+/*
+ * The following are the token types that will be supported in the language.
+ */
+
+const (
+	ILLEGAL   = "ILLEGAL" // used for marking unknown characters
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMA      = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
