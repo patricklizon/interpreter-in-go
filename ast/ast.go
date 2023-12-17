57f1e7f5487fca4ffa3b6c3377dc9cc103100f08
Patryk Lizoń <lizon.patryk@gmail.com>
2023-12-17 17:57:38 +0100

    feat: Add String methods to statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/patricklizon/interpreter-in-go/token"
+import (
+	"bytes"
+
+	"github.com/patricklizon/interpreter-in-go/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -27,6 +32,10 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -37,6 +46,21 @@ func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(ls.TokenLiteral() + " ")
+	buffer.WriteString(ls.Name.String() + " = ")
+
+	if ls.Value != nil {
+		buffer.WriteString(ls.Value.TokenLiteral())
+	}
+
+	buffer.WriteString(";")
+
+	return buffer.String()
 }
 
 type ReturnStatement struct {
@@ -50,6 +74,40 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+func (rs *ReturnStatement) String() string {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		buffer.WriteString(rs.ReturnValue.String())
+	}
+
+	buffer.WriteString(";")
+
+	return buffer.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
 type Program struct {
 	Statements []Statement
 }
@@ -60,4 +118,14 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+func (p *Program) String() string {
+	var buffer bytes.Buffer
+
+	for _, s := range p.Statements {
+		buffer.WriteString(s.String())
+	}
+
+	return buffer.String()
 }
